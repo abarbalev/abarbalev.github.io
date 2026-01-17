06b524398008b78cd09e1ada30a6770b73386353
abarbalev <levpaypay@gmail.com>
2026-01-17 23:04:56 +0200

    Add files via upload

---
+++ bible-config.js
@@ -0,0 +1,9 @@
+﻿// FIREBASE CONFIG (SPECIFIC TO BIBLE APP)
+const firebaseConfig = {
+    apiKey: "AIzaSyDlAJu_L2xKtxb21HDByXwRVZEQhEeAgW4",
+    authDomain: "bagrut-bible.firebaseapp.com",
+    projectId: "bagrut-bible",
+    storageBucket: "bagrut-bible.firebasestorage.app",
+    messagingSenderId: "4724909210",
+    appId: "1:4724909210:web:bbca1e6b68a658c6066b23"
+};
---
+++ index-config.js
@@ -0,0 +1,10 @@
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyBLhUi6ATMdHIMg-4Gqqn2UBJNg68gVXbE",
+    authDomain: "lev-sikom.firebaseapp.com",
+    databaseURL: "https://lev-sikom-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "lev-sikom",
+    storageBucket: "lev-sikom.firebasestorage.app",
+    messagingSenderId: "591606538772",
+    appId: "1:591606538772:web:ba4140ed8a60c6953dbd01",
+    measurementId: "G-90DZZSZP3M"
+};
